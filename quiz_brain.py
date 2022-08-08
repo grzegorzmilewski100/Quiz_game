84be9caedd8962cd035f4fa98ebaba742b0bc858
Grzegorz Milewski <grzegorzmilewski@MacBook-Air-Grzegorz.local>
2022-08-08 19:04:56 +0200

    Adding the method which checks if the users answer is correct

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_question(self):
         return self.question_number < len(self.question_list)
@@ -10,6 +11,19 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"{self.question_number}: {current_question.text} (True/False): ")
+        usesr_answer = input(f"{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(usesr_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("Your answer is correct.")
+        else:
+            print("Your answer is incorrect.")
+        print(f"The correct answer is: {correct_answer}")
+        print(f"Your current score is {self.score}/{self.question_number}")
+
+
+
 
 
